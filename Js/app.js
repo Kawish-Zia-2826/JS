fe213b53fea24763b5d2b1e75a29c285f6642ac6
Kawish Zia <kawishzia2826@gmail.com>
2024-08-25 12:09:45 +0500

    Add files via upload

---
+++ Js/app.js
@@ -0,0 +1,173 @@
+// //-----------------------------------------------------Assignment01(Alert)---------------------------------------------------------------------------
+
+// // -------------------------------------------------------------TAsk No 1-----------------------------------------------------------------------------
+// // Write a script to greet your website visitor using JS alert
+// // box.
+// // 2. Write a script to display following message on your web
+
+//  alert("please Enter a Valid password");
+
+// // -------------------------------------------------------------TAsk No 2-----------------------------------------------------------------------------
+
+// // Write a script to display following message on your web
+// // page: (Hint : Use line break)
+//  alert("Wlcome to JS Land \n HappyCoding!");
+
+// // -------------------------------------------------------------TAsk No 3-----------------------------------------------------------------------------
+
+// // 4. Write a script to display following messages in sequence:
+//  alert("Welcome To JS Land");
+//  alert("Happy Coding!");
+
+// // -------------------------------------------------------------TAsk No 4----------------------------------------------------------------------------
+
+// // Generate the following message through browser’s
+// // developer console
+//  alert("hello i can use Js through my web browser console");
+
+
+// //-----------------------------------------------------------------------Assignment02(Variable for string)-----------------------------------------------
+// //----------------------------------------------------------------------Task No 01----------------------------------------------------------------------1. Declare a variable called username.
+// // 2. Declare a variable called myName & assign to it a string
+// // that represents your Full Name.
+// // 3. Write script to
+// // a) Declare a JS variable, titled message.
+// // b) Assign “Hello World” to variable message
+// // c) Display the message in alert box.
+// // 4. Write a script to save student’s bio data in JS variables and
+// // show the data in alert boxes
+// var username;
+// var myname = "kawish zia";
+// var message = "Hello World"
+// username = "kawish"
+// var age = "18";
+// var course = "Web and App"
+// alert(message);
+// alert(username );
+// alert(myname );
+// alert(age );
+// alert(course );
+
+
+// //----------------------------------------------------------------------Task No 02----------------------------------------------------------------------
+
+// // Write a script to display the following alert using one JS
+// // variable:
+
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+// //----------------------------------------------------------------------Task No 03----------------------------------------------------------------------
+
+// // Declare a variable called email and assign to it a string that
+// // represents your Email Address(e.g. example@example.com).
+// // Show the blow mentioned message in an alert box.(Hint: use
+// // string concatenation)
+
+// var email  = "Kawihszia2826@gmail.com"
+// alert(email)
+// //----------------------------------------------------------------------Task No 04----------------------------------------------------------------------
+
+// // Declare a variable called book & give it the value “A
+// // smarter way to learn JavaScript”. Display the following
+// // message in an alert box
+// var book = "A smarter way to lern js";
+// alert("i am trying to lern from the " + book)
+
+
+
+
+
+// //--------------------------------------------------------Assignment No (03)VARIABLES FOR NUMBERS--------------------------------------------------
+
+// //----------------------------------------------------------------------Task No 01----------------------------------------------------------------------
+// //1. Declare a variable called age & assign to it your age. Show
+// // your age in an alert box.
+
+// var age = 18;
+// alert("my age is" + age)
+// //----------------------------------------------------------------------Task No 02----------------------------------------------------------------------
+
+// // //2. Declare & initialize a variable to keep track of how many
+// // times a visitor has visited a web page. Show his/her
+// // number of visits on your web page. For example: “You
+// // have visited this site N times”.
+// //----------------------------------------------------------------------Task No 03----------------------------------------------------------------------
+
+// var visit = 14;
+// alert("you are visit our web "  + visit);
+// //Declare a variable called birthYear & assign to it your
+// // birth year. Show the following message in your browser:
+
+// var birthYear = 2006;
+// document.write("my birth year is" +  birthYear);
+// //----------------------------------------------------------------------Task No 04----------------------------------------------------------------------
+
+// // 4. A visitor visits an online clothing store
+// // www.xyzClothing.com . Write a script to store in variables
+// // the following information:
+// // a. Visitor’s name
+// // b. Product title
+// // c. Quantity i.e. how many products a visitor wants to
+// // order
+// // Show the following message in your browser: “John
+// // Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+// var xyz = "XYZ clothing store";
+// var visitorsName = "JhonDoe";
+// var productTiitle = "T-Shirt";
+// var order = 5;
+// document.write(visitorsName + " "+ productTiitle + " " + order + " " + xyz);
+
+
+
+// //---------------------------------------------------------------------JS Assignment 04(VARIABLE NAMES: LEGAL & ILLEGAL)
+// //----------------------------------------------------------------------Task No 01----------------------------------------------------------------------
+// // 1. Declare 3 variables in one statement.
+// // 2. Declare 5 legal & 5 illegal variable names.
+// // 3. Display this in your browser
+// // a) A heading stating “Rules for naming JS variables”
+// // b) Variable names can only contain ______, ______,
+// // ______ and ______.
+// // For example $my_1stVariable
+// // c) Variables must begin with a ______, ______ or
+// // _____. For example $name, _name or name
+// // d) Variable names are case _________
+// // e) Variable names should not be JS _________
+
+// //----------------------------------------------------------------------Task No 01----------------------------------------------------------------------
+
+//  let abc;
+//  const abcd = 1;
+//  var abcde;
+
+
+
+// //----------------------------------------------------------------------Task No 02----------------------------------------------------------------------
+
+// var Geeks;
+// var Geeks_for_Geeks;
+// var $Geeks;
+// var _Geeks;
+// var Geeks123;
+// //var Geeks-for-Geeks;  // Hyphens not allowed
+// //var 123Geeks;  // Can not start with numbers
+// //var Geeks for Geeks;  // Spaces not allowed
+// //----------------------------------------------------------------------Task No 03----------------------------------------------------------------------
+// // 3. Display this in your browser
+// document.write("A heading stating “Rules for naming JS variables” b) Variable names can only contain number, $,underscore For example $my_1stVariable c) Variables must begin with a _letter,$, underscore. For example  $name, _name or name $name, _name or name d) Variable names are case __sensetivee) Variable names should not be JS keyword");
+
+
+//----------------------------------------------------------------------Asignment  No 05(MATH EXPRESSIONS)----------------------------------------------------------------------
+// //----------------------------------------------------------------------Task No 01----------------------------------------------------------------------
+// Write a program that take two numbers & add them in a
+// new variable. Show the result in your browser.
+
+var num1 = 3;
+var num2 = 5;
+result = num1 + num2;
+document.write("sum of " + num1 +  "and" + num2 + "= " + result);
+
+// //----------------------------------------------------------------------Task No 02----------------------------------------------------------------------
+
+
+
